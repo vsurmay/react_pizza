e7be130573da62eaa4d141c4691932a97ac4a826
vsurmay <vsurmaj510@gmail.com>
2023-05-09 19:06:12 +0300

    fix chunge name

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,14 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Home from "../pages/Home/Home";
-// import Basket from "../pages/Basket/Basket";
 import NotFound from "../pages/NotFound/NotFound";
-// import ProductPage from "../pages/ProductPage/ProductPage";
 import { Suspense, lazy } from "react";
 import FadeLoad from "../components/Loader/FadeLoad";
 
-const Basket = lazy(() => import("../pages/Basket/Basket"));
-const ProductPage = lazy(() => import("../pages/ProductPage/ProductPage"));
+const Basket = lazy(
+  () => import(/* webpackChunkName: "Basket" */ "../pages/Basket/Basket")
+);
+const ProductPage = lazy(
+  () =>
+    import(
+      /* webpackChunkName: "ProductPage" */ "../pages/ProductPage/ProductPage"
+    )
+);
 
 export const router = createBrowserRouter([
   {
